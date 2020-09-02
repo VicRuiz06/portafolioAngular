0e0c6226d7e7ced88f02b7c74a004ea666266e0d
Victor Ruiz <victor.ruiz@escotel.mx>
2020-09-02 11:33:27 -0500

    Ya tiene buscador

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { InfoPaginaService } from '../../services/info-pagina.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,9 +10,19 @@ import { InfoPaginaService } from '../../services/info-pagina.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor( public _servicio: InfoPaginaService) { }
+  constructor( public _servicio: InfoPaginaService,
+
+  				private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  buscarProducto( termino: string ){
+  	if( termino.length < 1) { /*valida que se busque en vacío*/
+  		return;
+  	}
+  		this.router.navigate(['/search', termino]);
+  		
   }
 
 }
